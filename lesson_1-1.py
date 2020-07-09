d07c01f6af2e719b40191fc12af51bcf49be4e4f
Ekaterina <esokolova@mindthedigital.com>
2020-07-09 17:56:27 +0300

    calculator added

--- lesson_1-1.py
+++ lesson_1-1.py
@@ -1,9 +1,25 @@
 name = input("Your name? ")
 city = input("Where do you live? ")
 age = input("How old are you? ")
-print(f"Hello! My name is {name}, i live in {city}, i'm {age} years old.")
-num1 = input("Whrite the 1st number: ")
-num2 = input("Write the 2nd number: ")
-print(num1)
-print(num2)
-print("Summ num1 + num2 = ", int(num1) + int(num2))
+print(f"Hello! Your name is {name}, you live in {city}, you are {age} years old.\nLet's calculate a bit:")
+while True:
+    a = float(input("Enter the 1st number. \na="))
+    b = float(input("Enter the 2nd number. \nb="))
+    operation = input("Enter an operator (+, -, /, *): ")
+    if operation == str("+"):
+        print("a + b = ", a + b)
+    elif operation == str("-"):
+        print("a - b = ", a - b)
+    elif operation == str("/"):
+        if b == 0:
+            print("Zero division.")
+            b = float(input("Enter other value of b"))
+        print("a / b = ", a / b)
+    elif operation == str("*"):
+        print("a * b = ", a * b)
+    else:
+        print("Sorry, it is not working yet")
+    reply = input("Do you want to count something else? y/n ")
+    if reply == "y":
+        continue
+    break
