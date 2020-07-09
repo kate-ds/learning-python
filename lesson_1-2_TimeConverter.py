3866f35d7af134f437aa2ab30a05513db2657d22
Ekaterina <esokolova@mindthedigital.com>
2020-07-09 20:15:00 +0300

    Bugfixing

--- lesson_1-2_TimeConverter.py
+++ lesson_1-2_TimeConverter.py
@@ -3,7 +3,6 @@ while True:
     time = input("Enter time in seconds: ")
     if not time.isdecimal():
         print("It's a wrong time.")
-        # continue
     else:
         time = int(time)
         if time >= 86400:
@@ -15,7 +14,7 @@ while True:
             print(f"It's {hour:02}:{minutes:02}:{seconds:02}")
     quit = input("Do you want to continue? (y/n):")
     if str(quit) == "y":
-        print("Have a nice day!")
         continue
     else:
+        print("Have a nice day!")
         break
