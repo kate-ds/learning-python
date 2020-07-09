f4c104e01f3da65fae191d89fe2e16d1811e845c
Ekaterina <esokolova@mindthedigital.com>
2020-07-09 19:40:42 +0300

    add loop

--- lesson_1-2_TimeConverter.py
+++ lesson_1-2_TimeConverter.py
@@ -1,12 +1,21 @@
-time = input("Enter time in seconds: ")
-if not time.isdecimal():
-    print("It's a wrong time.")
-else:
-    time = int(time)
-    if time >= 86400:
-        print("It is more than one day. Try again")
+
+while True:
+    time = input("Enter time in seconds: ")
+    if not time.isdecimal():
+        print("It's a wrong time.")
+        # continue
     else:
-        hour = time // 3600
-        minutes = time % 3600 // 60
-        seconds = time % 3600 % 60
-        print(f"It's {hour:02}:{minutes:02}:{seconds:02}")
+        time = int(time)
+        if time >= 86400:
+            print("It is more than one day. Try again")
+        else:
+            hour = time // 3600
+            minutes = time % 3600 // 60
+            seconds = time % 3600 % 60
+            print(f"It's {hour:02}:{minutes:02}:{seconds:02}")
+    quit = input("Do you want to continue? (y/n):")
+    if str(quit) == "y":
+        print("Have a nice day!")
+        continue
+    else:
+        break
