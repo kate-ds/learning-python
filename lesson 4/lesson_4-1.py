dff0f89acc6961927d3324aa57bd92d0a393487a
Ekaterina <esokolova@mindthedigital.com>
2020-07-20 23:29:50 +0300

    Fix task 1

--- lesson 4/lesson_4-1.py
+++ lesson 4/lesson_4-1.py
@@ -10,12 +10,14 @@ file_name, hours_worked, rate, bonus = argv
 
 try:
     salary = float(hours_worked) * float(rate) + float(bonus)
-
-    print(f"Hours worked: {hours_worked} hours")
-    print(f"Rate: {rate} rub/hour")
-    print(f"Bonus: {bonus} rub")
-    print(f"Your salary: {salary} rub")
+    if float(hours_worked) >= 0 and float(rate) >= 0 and float(bonus) >= 0:
+        print(f"Hours worked: {hours_worked} hours")
+        print(f"Rate: {rate} rub/hour")
+        print(f"Bonus: {bonus} rub")
+        print(f"Your salary: {salary} rub")
+    else:
+        print("Incorrect values")
 except ValueError:
-    print("Incorrect Values!")
+    print("Incorrect values!")
 
 
