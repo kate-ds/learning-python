b3ee31d888ea0ac04c7dffcec487022988670bab
Ekaterina <esokolova@mindthedigital.com>
2020-07-21 16:00:08 +0300

    Add task 6b

---
+++ lesson 4/lesson_4-6b.py
@@ -0,0 +1,28 @@
+"""
+6. Реализовать два небольших скрипта:
+
+б) итератор, повторяющий элементы некоторого списка, определенного заранее.
+"""
+
+from itertools import cycle
+
+my_list = ["Don't", "worry!", "Be", "happy"]
+i = 0
+for word in cycle(my_list):
+    if i > 8:
+        break
+    print(word)
+    i += 1
+
+# ---------------------------------------------------------------------------
+
+einstein_said = ["Example",  "isn`t", "another way", "to teach", "it is", "the only way", "to teach."]
+iter = cycle(albert_einstein_said)
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+print(next(iter))
+
