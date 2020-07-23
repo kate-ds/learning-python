e360ca3886c9ed9418b5500108b84f2d85d1bdd8
Ekaterina <esokolova@mindthedigital.com>
2020-07-23 20:48:21 +0300

    Add task 1

---
+++ lesson 5/lesson_5-1.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+with open("user_text.txt", "w", encoding="utf-8") as user_text:
+    print("Enter file contents:")
+    for line in stdin:
+        # if line == " \n":
+        if line.isspace():
+            break
+        user_text.write(line)
+
+# -------------------------------------------------------------------------------------
+
+print("Enter:")
+user_input = ""
+for line in stdin:
+    user_input += line
+    if line.isspace():
+        break
+print(user_input, end="\n")
+with open("user_text0.txt", "w", encoding="utf-8") as user_text:
+    user_text.write(user_input[:-1])
+
+# -------------------------------------------------------------------------------------
+
+print("Enter:")
+user_input: list = []
+for line in stdin:
+    user_input.append(line)
+    if line.isspace():
+        break
+
+with open("user_text.txt", "w", encoding="utf-8") as f:
+    f.write("".join(user_input[:-1]))
+
+
