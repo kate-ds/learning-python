5a247da33f785a596a7701b64e962c90eebad909
Ekaterina <esokolova@mindthedigital.com>
2020-07-24 14:58:45 +0300

    Add task 5

--- lesson 5/lesson_5-5.py
+++ lesson 5/lesson_5-5.py
@@ -1,29 +1,39 @@
 """
-3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников
-и величину их окладов.
-Определить, кто из сотрудников имеет оклад менее 20 тыс.,
-вывести фамилии этих сотрудников.
-Выполнить подсчет средней величины дохода сотрудников.
+5. Создать (программно) текстовый файл, записать в него программно набор чисел,
+разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и
+выводить ее на экран.
 """
+from sys import exit
+
+print("this program will generate a sequence of numbers and calculate their sum")
+try:
+    n1 = int(input("Enter the first number: "))
+    n2 = int(input("Enter the last number: "))
+except ValueError:
+    print("Numbers should be only integers")
+    exit(1)
+
+with open("numbers.txt", "w", encoding="utf-8") as f:
+    num: list = [str(i) for i in range(n1, n2) if i % 2 == 1]
+    num: str = " ".join(num)
+    f.write(num)
+
+with open("numbers.txt", "r", encoding="utf-8") as f:
+    numbers: str = f.read()
+numbers_list: list = numbers.split(" ")
 
 try:
-    with open("people.txt", "r", encoding="utf-8") as f:
-        file_list = f.readlines()
-
-    people = []
-    for e in file_list:
-        people.append({
-            "name": e.split(" ")[0],
-            "salary": float(e.split(" ")[1][:-1])
-        })
-    print("Guys with salary lesser than 20,000:", ", ".join([e["name"] for e in people if e["salary"] < 20000]))
-
-    salary_sum = 0
-    for e in people:
-        salary_sum += e["salary"]
-    average_salary = salary_sum / len(people)
-    print("Average salary: %.2f" % average_salary)
-
+    numbers_list: list = [int(i) for i in numbers_list]
+    print(numbers_list)
+    sum = 0
+    for i in numbers_list:
+        sum += i
+    print("Sum: ", sum)
+except ValueError:
+    print("File should contain only numbers")
 except IOError:
-    print("Error!")
+    print("File opening error!")
+
+
+
 
